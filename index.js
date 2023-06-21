5299192ff1619067263924d91e270aa8f82a1404
mertee05 <131835458+mertee05@users.noreply.github.com>
2023-06-21 10:39:00 -0700

    initial commit
    Building the functionality and barebones

---
+++ index.js
@@ -0,0 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let generatedPw1Element = document.getElementById("generated-pw1");
+let generatedPw2Element = document.getElementById("generated-pw2");
+
+function handleGeneratePwClick() {
+  let generatedPW1 = generatePW();
+  let generatedPW2 = generatePW();
+  generatedPw1Element.textContent = generatedPW1;
+  generatedPw2Element.textContent = generatedPW2;
+}
+
+function generatePW() {
+  let password = "";
+  for (let i = 0; i < 16; i++) {
+    let randCharacter =
+      characters[Math.floor(Math.random() * characters.length)];
+    password += randCharacter;
+  }
+  return password;
+}
